ea625c664e6568e762daafe83e3535a162d06da5
Flacial <35906419+Flacial@users.noreply.github.com>
2021-03-31 01:14:43 +0400

    Change chakra-ui use-disclosure to the reducer one

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,25 +4,17 @@ import { Box } from '@chakra-ui/react';
 import React, { Suspense, lazy } from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { Presets } from 'react-component-transition';
 import Help from '../help-button/help-button.component';
 import ThemeButton from '../theme-button/theme-button.component';
-import SearchButtonHeader from '../search-button/search-button.component';
-import ErrorBoundary from '../error-boundary/error-boundary.component';
-
-const PopUpSearchBar = lazy(() => import('../popup-search-bar/popup-search-bar.component'));
+// const PopUpSearchBar = lazy(() => import('../popup-search-bar/popup-search-bar.component'));
+// import SearchButtonHeader from '../search-button/search-button.component';
+// import ErrorBoundary from '../error-boundary/error-boundary.component';
+const SearchButtonHeader = lazy(() => import('../search-button/search-button.component'));
 
 const NavBar = ({
-  onOpen2,
   InputFiledSearchBarPopUp,
   location,
-  ReturnedWord,
-  getWords,
-  setWordsLoaded,
-  HandleBackButtonClick,
-  onToggle2,
-  onClose2,
-  isOpen2,
-  getInputValue,
   WordsLoaded,
 }) => (
   <Box
@@ -38,10 +30,10 @@ const NavBar = ({
         <>
           <Presets.TransitionFade>
             <Suspense fallback={null}>
-          <SearchButtonHeader
+              <SearchButtonHeader
                 WordsLoaded={WordsLoaded}
-            InputFiledSearchBarPopUp={InputFiledSearchBarPopUp}
-          />
+                InputFiledSearchBarPopUp={InputFiledSearchBarPopUp}
+              />
             </Suspense>
           </Presets.TransitionFade>
         </>
@@ -52,8 +44,4 @@ const NavBar = ({
   </Box>
 );
 
-const mapStateToProps = ({ words }) => ({
-  ReturnedWord: words.ReturnedWord,
-});
-
-export default connect(mapStateToProps, null)(withRouter(NavBar));
+export default withRouter(NavBar);
