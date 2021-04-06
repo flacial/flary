747ac94f328499c838c27ec018ca967e3d67cb82
Flacial <35906419+Flacial@users.noreply.github.com>
2021-04-07 01:28:14 +0400

    Made a folder for disabled functions

--- src/components/tabs/tabs.component.jsx
+++ src/components/tabs/tabs.component.jsx
@@ -61,44 +61,6 @@ const WordsTabs = ({
     return false;
   };
 
-  // const HandleKeyDownTabsButtonSwitch = (event, AvailableWordType) => {
-  //   switch (event.key) {
-  //     case '!': // Noun 1
-  //       if (TabListElement?.current?.childElementCount >= 2) {
-  //         event.preventDefault();
-  //         return (!AvailableWordType.noun && AdjectiveTabButton?.current?.click()) || NounTabButton?.current?.click();
-  //       }
-  //       break;
-  //     case '@': // Verb 2
-  //       event.preventDefault();
-  //       return VerbTabButton?.current?.click();
-  //     case '#': // Adjective 3
-  //       if (AvailableWordType?.noun && AvailableWordType?.verb) {
-  //         event.preventDefault();
-  //         return AdverbTabButton?.current?.click() || PhraseTabButton?.current?.click() || AdjectiveTabButton?.current?.click();
-  //       }
-  //       break;
-  //     case '$': // Phrase 4
-  //       event.preventDefault();
-  //       return AdverbTabButton?.current?.click() || PhraseTabButton?.current?.click();
-  //     case '%': // Adverb 5
-  //       if (AvailableWordType?.phrase) {
-  //         event.preventDefault();
-  //         AdverbTabButton?.current?.click();
-  //       }
-  //       break;
-  //     default:
-  //       break;
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   document.addEventListener('keydown', (e) => HandleKeyDownTabsButtonSwitch(e, AvailableWordType));
-  //   return () => {
-  //     document.removeEventListener('keydown', (e) => HandleKeyDownTabsButtonSwitch(e, AvailableWordType));
-  //   };
-  // }, [AvailableWordType]);
-
   const ThesaurusTabPanel = (WordsArray) => (
     <WordsContainer boxShadow={boxShadow} gradientbg={gradientbg} ml={['0', '9em', null]} mr={['0', '9em', null]} marginTop={['3', null, null]}>
       <Presets.TransitionFade>
