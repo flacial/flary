4c888beeb66855048f1f1555a42df7bd680beddd
flacial <ruthemiumwithdiamond@protonmail.com>
2021-08-14 04:44:24 +0400

    style: Github button animate on parent hover

--- src/components/github-button/github-button.component.jsx
+++ src/components/github-button/github-button.component.jsx
@@ -6,8 +6,8 @@ const GitHubButton = () => {
   const focusBorderColorInput = useColorModeValue({ boxShadow: '0 0 0 3px #3B82F6' }, { boxShadow: '0 0 0 3px orange' });
   const color = useColorModeValue('#252d3d', '#edf2f7');
   return (
-    <Button rel="noopener" title="Github Button" as="a" href="https://www.github.com/flacial/flary" _focus={focusBorderColorInput} focusBorderColor={focusBorderColorInput} boxShadow="lg" target="_blank" outline="none" outlineColor="initial" style={{ outlineStyle: 'none' }} m="4" mr="-1" rounded="xl">
-      <Logo className="rainbow" width="1.2em" height="1.2em" fill={color} />
+    <Button className="rainbow" rel="noopener" title="Github Button" as="a" href="https://www.github.com/flacial/flary" _focus={focusBorderColorInput} focusBorderColor={focusBorderColorInput} boxShadow="lg" target="_blank" outline="none" outlineColor="initial" style={{ outlineStyle: 'none' }} m="4" mr="-1" rounded="xl">
+      <Logo className="rainbow__logo" width="1.2em" height="1.2em" fill={color} />
     </Button>
   );
 };
